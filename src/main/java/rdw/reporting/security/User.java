b7a0a90f8feb849329fed9519a2775092889075b
esotrope <llunsford@fairwaytech.com>
2017-02-17 11:41:10 -0800

    removes use of lombock and replaces with immutables

--- src/main/java/rdw/reporting/security/User.java
+++ src/main/java/rdw/reporting/security/User.java
@@ -1,28 +1,26 @@
 package rdw.reporting.security;
 
-import lombok.*;
+import org.immutables.value.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
-@Getter
-@Builder
-@AllArgsConstructor
-@EqualsAndHashCode(of = {"id"})
-public class User implements UserDetails {
+@Value.Immutable
+@Value.Style(get = {"get*", "is*"})
+public interface User extends UserDetails {
 
-	private String id;
-	private String email;
-	private String givenName;
-	private List<Tenancy> tenancyChain;
-	private String password;
-	private String username;
-	private Collection<GrantedAuthority> authorities;
-	private boolean accountNonExpired;
-	private boolean accountNonLocked;
-	private boolean credentialsNonExpired;
-	private boolean enabled;
+	String getId();
+	@Value.Auxiliary String getEmail();
+	@Value.Auxiliary String getGivenName();
+	@Value.Auxiliary List<Tenancy> getTenancyChain();
+	@Value.Auxiliary String getPassword();
+	@Value.Auxiliary String getUsername();
+	@Value.Auxiliary Collection<GrantedAuthority> getAuthorities();
+	@Value.Auxiliary boolean isAccountNonExpired();
+	@Value.Auxiliary boolean isAccountNonLocked();
+	@Value.Auxiliary boolean isCredentialsNonExpired();
+	@Value.Auxiliary boolean isEnabled();
 
 }
