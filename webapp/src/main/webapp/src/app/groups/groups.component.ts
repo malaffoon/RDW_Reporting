6e431404133d2a143bb63aef478c88bd7360ed00
Brandon Savage <bsavage@fairwaytech.com>
2017-05-10 11:25:56 -0700

    Added filtering on groups name in the my groups widget.  Updated standalone service to latest api model changes.  Added more test data for groups.

--- webapp/src/main/webapp/src/app/groups/groups.component.ts
+++ webapp/src/main/webapp/src/app/groups/groups.component.ts
@@ -8,12 +8,19 @@ import {DataService} from "../shared/data.service";
 export class GroupsComponent implements OnInit {
 
   private groups = [];
+  private filteredGroups = [];
+  private searchTerm : string;
 
   constructor(private service: DataService) {}
 
   ngOnInit() {
     this.service.getGroups().subscribe(groups => {
       this.groups = groups;
+      this.filteredGroups = groups;
     })
+  }
+
+  onSearchChange(event) {
+    this.filteredGroups = this.groups.filter( x => x.name.toUpperCase().indexOf(this.searchTerm.toUpperCase()) >= 0)
   }
 }
