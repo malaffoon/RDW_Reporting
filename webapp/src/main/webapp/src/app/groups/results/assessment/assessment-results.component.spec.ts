babc6a097a368fd97acf80ce2c14c6bd072898ee
Brandon Savage <bsavage@fairwaytech.com>
2017-05-17 16:12:37 -0700

    Updated data so sessions can vary.  Added sessions to the assessment-results component which is selectable / delectable.  Assessment-results are now collapsible.   Updated unit test.  Updated styles of buttons.

--- webapp/src/main/webapp/src/app/groups/results/assessment/assessment-results.component.spec.ts
+++ webapp/src/main/webapp/src/app/groups/results/assessment/assessment-results.component.spec.ts
@@ -1,25 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AssessmentResultsComponent } from './assessment-results.component';
+import { APP_BASE_HREF } from "@angular/common";
+import { TranslateModule } from "@ngx-translate/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { AppModule } from "../../../app.module";
+import { Component } from "@angular/core";
 
 describe('AssessmentResultsComponent', () => {
   let component: AssessmentResultsComponent;
-  let fixture: ComponentFixture<AssessmentResultsComponent>;
+  let fixture: ComponentFixture<TestComponentWrapper>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AssessmentResultsComponent ]
-    })
-    .compileComponents();
-  }));
+      imports: [ TranslateModule.forRoot(), HttpModule, FormsModule ],
+      declarations: [ TestComponentWrapper, AssessmentResultsComponent ],
+      providers: [ { provide: APP_BASE_HREF, useValue: '/' }]
+    }).compileComponents();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AssessmentResultsComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestComponentWrapper);
+    component = fixture.debugElement.children[0].componentInstance;
+
     fixture.detectChanges();
-  });
+
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
+
+@Component({
+  selector: 'test-component-wrapper',
+  template: '<assessment-results [assessment]="assessment"></assessment-results>'
+})
+class TestComponentWrapper {
+  assessment = { sessions: [] }
+}
--- webapp/src/main/webapp/src/app/groups/results/assessment/assessment-results.component.ts
+++ webapp/src/main/webapp/src/app/groups/results/assessment/assessment-results.component.ts
@@ -1,19 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
   selector: 'assessment-results',
-  templateUrl: './assessment-results.component.html',
+  templateUrl: './assessment-results.component.html'
 })
+
 export class AssessmentResultsComponent implements OnInit {
 
   @Input()
   assessment;
-  
-  constructor() { }
 
   ngOnInit() {
-    console.log(this.assessment);
   }
-
-
 }
