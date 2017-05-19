b167b9c995f45e345cd107132eae7f0e9198f80f
Brandon Savage <bsavage@fairwaytech.com>
2017-05-19 15:28:10 -0700

    Just returning the en.json for now when running against local api now until the api implementation for translations is complete.

--- webapp/src/main/webapp/src/app/app.module.ts
+++ webapp/src/main/webapp/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { PadStartPipe } from "./shared/pad-start.pipe";
 import { AppComponent } from "./app.component";
 import { DataService } from "./shared/data.service";
 import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
-import { TranslateHttpLoader } from "@ngx-translate/http-loader"
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { environment } from "../environments/environment";
 import { standaloneProviders } from "./standalone/standalone.service";
 import { GroupStudentsComponent } from "./group-students/group-students.component";
@@ -23,25 +23,27 @@ import { StudentExamsCategorizedComponent } from "./student-exams/student-exams-
 import { GroupExamItemComponent } from "./group-exam-item/group-exam-item.component";
 import { StudentExamReportComponent } from "./student-exam-report/student-exam-report.component";
 import { IabReportComponent } from "./student-exam-report/iab-report.component";
-import { AdminSearchComponent } from './admin-search/admin-search.component';
+import { AdminSearchComponent } from "./admin-search/admin-search.component";
 import { GroupResolve } from "./groups/group.resolve";
 import { StudentExamsResolve } from "./student-exams/student-exam.resolve";
 import { StudentExamItemsResolve } from "./student-exam-items/student-exam-items.resolve";
 import { GroupExamItemResolve } from "./group-exam-item/group-exam-item.resolve";
 import { BreadcrumbsComponent } from "./breadcrumbs/breadcrumbs.component";
-import { DataTableModule, SharedModule } from 'primeng/primeng';
+import { DataTableModule, SharedModule } from "primeng/primeng";
 import { SubjectPipe } from "./shared/subject.pipe";
-import { GroupResultsComponent } from './groups/results/group-results.component';
+import { GroupResultsComponent } from "./groups/results/group-results.component";
 import { BsDropdownModule } from "ngx-bootstrap";
 import { GroupsResolve } from "./groups/groups.resolve";
 import { CachingDataService } from "./shared/cachingData.service";
 import { AssessmentsResolve } from "./groups/results/assessments.resolve";
 import { SchoolYearPipe } from "./shared/schoolYear.pipe";
-import { AssessmentResultsComponent } from './groups/results/assessment/assessment-results.component';
+import { AssessmentResultsComponent } from "./groups/results/assessment/assessment-results.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 export function createTranslateLoader(http: Http) {
-  return new TranslateHttpLoader(http, '/api/translations/', '');
+  // return new TranslateHttpLoader(http, '/api/translations/', '');
+  // until api implementation is complete, use local .json
+  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
 }
 
 @NgModule({
