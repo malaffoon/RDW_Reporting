30f6d59ef8c9e0e47f3b4ea5de9a4aca6527525e
Brandon Savage <bsavage@fairwaytech.com>
2017-05-18 17:05:37 -0700

    Added students results table and enabled filtering on this table by toggling the sessions buttons.  Added unit tests for additional functionality of the assessment results component.

--- webapp/src/main/webapp/src/app/groups/results/assessment/assessment-results.component.ts
+++ webapp/src/main/webapp/src/app/groups/results/assessment/assessment-results.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 
 export class AssessmentResultsComponent implements OnInit {
+  private _assessment;
+  private _examSessions = [];
 
   @Input()
-  assessment;
+  set assessment(assessment) {
+    this._assessment = assessment;
+
+    if (this._assessment.sessions && this._assessment.sessions.length > 0) {
+      // Select the first session by default.
+      this._assessment.sessions[ 0 ].filter = true;
+      this.updateExamSessions(this._assessment.sessions[ 0 ]);
+    }
+  }
+
+  get assessment() {
+    return this._assessment;
+  }
+
+  get examSessions() {
+    return this._examSessions;
+  }
 
   ngOnInit() {
+  }
+
+  toggleSession(session) {
+    session.filter = !session.filter;
+    this.updateExamSessions(session);
+  }
+
+  private updateExamSessions(session) {
+    // create copy
+    let examSessions = this._examSessions.filter(x => true);
+
+    if (session.filter)
+      session.exams.forEach(x => examSessions.push({ session: session, exam: x }));
+    else
+      session.exams.forEach(x => examSessions.splice(examSessions.findIndex(y => y.exam == x), 1))
+
+    this._examSessions = examSessions;
   }
 }
