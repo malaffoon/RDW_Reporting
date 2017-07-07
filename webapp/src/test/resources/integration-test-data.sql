aa17a0955470d12323b8b9509ef290925ea22209
Mark Laffoon <laffoon@san.rr.com>
2017-07-07 08:06:35 -0700

    DWR-491, denormalize grade_code (#206)

--- webapp/src/test/resources/integration-test-data.sql
+++ webapp/src/test/resources/integration-test-data.sql
@@ -21,14 +21,14 @@ insert into student (id, ssid, last_or_surname, first_name, gender_id, gender_co
   (-1, 'student1_ssid', 'student1_lastName', 'student1_firstName', -1, 'g1', '2017-01-01 00:00:00.000000', -1),
   (-2, 'student2_ssid', 'student2_lastName', 'student2_firstName', -1, 'g1', '2017-01-01 00:00:00.000000', -1);
 
-insert into asmt (id, type_id, natural_id, grade_id, subject_id, school_year, name, label, version,
+insert into asmt (id, type_id, natural_id, grade_id, grade_code, subject_id, school_year, name, label, version,
   claim1_score_code, claim2_score_code, claim3_score_code, claim4_score_code,
   min_score, cut_point_1, cut_point_2, cut_point_3, max_score, import_id) values
-  (-1, 1, 'ica1', -1, 1, 2017, 'ica1', 'ica1', 'v1', 'ica_claim1', 'ica_claim2', 'ica_claim3', 'ica_claim4', 100, 200, 300, 400, 500, -1),
-  (-2, 2, 'iab1', -1, 1, 2017, 'iab1', 'iab1', 'v1', null, null, null, null, 1, null, 2, null, 3, -1),
-  (-3, 3, 'sum1', -1, 1, 2017, 'sum1', 'sum1', 'v1', 'sum_claim1', 'sum_claim2', 'sum_claim3', null, 1000, 2000, 3000, 4000, 5000, -1);
+  (-1, 1, 'ica1', -1, 'g1', 1, 2017, 'ica1', 'ica1', 'v1', 'ica_claim1', 'ica_claim2', 'ica_claim3', 'ica_claim4', 100, 200, 300, 400, 500, -1),
+  (-2, 2, 'iab1', -1, 'g1', 1, 2017, 'iab1', 'iab1', 'v1', null, null, null, null, 1, null, 2, null, 3, -1),
+  (-3, 3, 'sum1', -1, 'g1', 1, 2017, 'sum1', 'sum1', 'v1', 'sum_claim1', 'sum_claim2', 'sum_claim3', null, 1000, 2000, 3000, 4000, 5000, -1);
 
-insert into exam (id, type_id, grade_id, student_id, school_id, opportunity, iep, lep, section504, economic_disadvantage,
+insert into exam (id, type_id, grade_id, grade_code, student_id, school_id, opportunity, iep, lep, section504, economic_disadvantage,
   school_year, asmt_id, asmt_version, completeness_id, completeness_code, administration_condition_id, administration_condition_code, session_id,
   scale_score, scale_score_std_err, performance_level, completed_at,
   claim1_category, claim1_scale_score, claim1_scale_score_std_err,
@@ -36,12 +36,12 @@ insert into exam (id, type_id, grade_id, student_id, school_id, opportunity, iep
   claim3_category, claim3_scale_score, claim3_scale_score_std_err,
   claim4_category, claim4_scale_score, claim4_scale_score_std_err,
   import_id) values
-  (-1, 1, -1, -1, -10, 0, 0, 0, 0, 0, 2017, -1, 'v1', 2, 'Complete', 1, 'Valid', 'session1', 2000, 20, 1, '2017-01-01 00:00:00.000000', 1, 100, 10, 2, 200, 20, 3, 300, 30, 4, 400, 40, -1),
-  (-2, 2, -2, -1, -10, 1, 0, 0, 0, 0, 2017, -2, 'v1', 2, 'Complete', 1, 'Valid', 'session2', 2100, 21, 2, '2017-01-01 00:00:00.000000', null, null, null, null, null, null, null, null, null, null, null, null, -1),
-  (-3, 3, -3, -1, -20, 2, 0, 0, 0, 0, 2017, -3, 'v1', 2, 'Complete', 1, 'Valid', 'session3', 2200, 22, 3, '2017-01-01 00:00:00.000000', 1, 1000, 100, 2, 2000, 200, 3, 3000, 300, null, null, null, -1),
-  (-4, 3, -1, -1, -30, 3, 0, 0, 0, 0, 2017, -3, 'v1', 2, 'Complete', 1, 'Valid', 'session4', 2300, 23, 1, '2017-01-02 00:00:00.000000', 1, 1100, 110, 2, 2100, 210, 3, 3100, 310, null, null, null, -1),
-  (-5, 3, -1, -1, -30, 4, 0, 0, 0, 0, 2017, -3, 'v1', 2, 'Complete', 1, 'Valid', 'session5', 2400, 24, 2, '2017-01-05 00:00:00.000000', 1, 1200, 120, 2, 2200, 220, 3, 3200, 320, null, null, null, -1),
-  (-6, 3, -1, -1, -30, null, 1, 1, 1, 1, 2017, -3, 'v1', 1, 'Partial', 2, 'Standardized', 'session6', null, null, null, '2017-01-03 00:00:00.000000', null, null, null, null, null, null, null, null, null, null, null, null, -1);
+  (-1, 1, -1, 'g1', -1, -10, 0, 0, 0, 0, 0, 2017, -1, 'v1', 2, 'Complete', 1, 'Valid', 'session1', 2000, 20, 1, '2017-01-01 00:00:00.000000', 1, 100, 10, 2, 200, 20, 3, 300, 30, 4, 400, 40, -1),
+  (-2, 2, -2, 'g2', -1, -10, 1, 0, 0, 0, 0, 2017, -2, 'v1', 2, 'Complete', 1, 'Valid', 'session2', 2100, 21, 2, '2017-01-01 00:00:00.000000', null, null, null, null, null, null, null, null, null, null, null, null, -1),
+  (-3, 3, -3, 'g3', -1, -20, 2, 0, 0, 0, 0, 2017, -3, 'v1', 2, 'Complete', 1, 'Valid', 'session3', 2200, 22, 3, '2017-01-01 00:00:00.000000', 1, 1000, 100, 2, 2000, 200, 3, 3000, 300, null, null, null, -1),
+  (-4, 3, -1, 'g1', -1, -30, 3, 0, 0, 0, 0, 2017, -3, 'v1', 2, 'Complete', 1, 'Valid', 'session4', 2300, 23, 1, '2017-01-02 00:00:00.000000', 1, 1100, 110, 2, 2100, 210, 3, 3100, 310, null, null, null, -1),
+  (-5, 3, -1, 'g1', -1, -30, 4, 0, 0, 0, 0, 2017, -3, 'v1', 2, 'Complete', 1, 'Valid', 'session5', 2400, 24, 2, '2017-01-05 00:00:00.000000', 1, 1200, 120, 2, 2200, 220, 3, 3200, 320, null, null, null, -1),
+  (-6, 3, -1, 'g1', -1, -30, null, 1, 1, 1, 1, 2017, -3, 'v1', 1, 'Partial', 2, 'Standardized', 'session6', null, null, null, '2017-01-03 00:00:00.000000', null, null, null, null, null, null, null, null, null, null, null, null, -1);
 
 -- items
 
