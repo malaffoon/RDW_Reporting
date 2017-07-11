2d8f2f1903999972aa8987de8741787c5d34e77e
Jeff Treuting <jtreuting@fairwaytech.com>
2017-07-11 14:43:56 -0700

    Added Angulartics2Module into the groups, school and grade and student modules

--- webapp/src/main/webapp/src/app/student/student.module.ts
+++ webapp/src/main/webapp/src/app/student/student.module.ts
@@ -14,6 +14,7 @@ import { DataTableModule } from "primeng/components/datatable/datatable";
 import { StudentHistoryIABTableComponent } from "./results/tables/student-history-iab-table.component";
 import { StudentHistoryICASummitiveTableComponent } from "./results/tables/student-history-ica-summitive-table.component";
 import { PopoverModule } from "ngx-bootstrap";
+import { Angulartics2Module } from 'angulartics2';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { PopoverModule } from "ngx-bootstrap";
     FormsModule,
     PopoverModule,
     ReactiveFormsModule,
-    SharedModule
+    SharedModule,
+    Angulartics2Module.forChild()
   ],
   exports: [ StudentComponent ],
   providers: [
