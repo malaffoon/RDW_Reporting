1c518e2c55f670736c01d61ff30d200820b095bd
Jeff Treuting <jtreuting@fairwaytech.com>
2017-07-11 16:36:08 -0700

    fixed sb-radio-button-list and sb-checkbox-list analytics wiring.  passing in label to those compoenetns so can log exactly which filter is clicked (otherwise would just have All, Yes, No for a lot of them and not if it is Migrant Status or 504 plan)

--- webapp/src/main/webapp/src/app/shared/sb-radio-button-list.component.ts
+++ webapp/src/main/webapp/src/app/shared/sb-radio-button-list.component.ts
@@ -6,26 +6,23 @@ import { Utils } from "./Utils";
   It is most typically used as:
   (All) [ (Option 1) (Option 2) ]
  */
-
-/*
-  TODO: Fix error and get angulartics working in this component
-  angulartics2On="click" [angularticsEvent]="analyticsEvent" [angularticsCategory]="analyticsCategory"
- */
-
 @Component({
   selector: 'sb-radio-button-list',
   template: `
     <div class="nested-btn-group btn-group-sm toggle-group" data-toggle="buttons">
       <label class="btn btn-primary" [ngClass]="{'active': model[property] == -1 }">
-        <input [value]="-1" [(ngModel)]="model[property]" type="radio" [name]="name" >{{ 'buttons.all' | translate }}
+        <input [value]="-1" [(ngModel)]="model[property]" type="radio" [name]="name" 
+                angulartics2On="click" [angularticsEvent]="analyticsEvent" 
+                [angularticsCategory]="analyticsCategory" [angularticsProperties]="{label: label + ': All'}">{{ 'buttons.all' | translate }}
       </label>
       <div class="btn-group">
         <label *ngFor="let value of values" class="btn btn-primary" [ngClass]="{'active': model[property] == value }">
-          <input [value]="value" [(ngModel)]="model[property]" type="radio" [name]="name">{{ enum + '.' + value | translate }}
+          <input [value]="value" [(ngModel)]="model[property]" type="radio" [name]="name"
+                angulartics2On="click" [angularticsEvent]="analyticsEvent" 
+                [angularticsCategory]="analyticsCategory" [angularticsProperties]="{label: label + ': ' + enum + '.' + value}">{{ enum + '.' + value | translate }}
         </label>
       </div>
     </div>`
-
 })
 export class SBRadioButtonComponent {
   // An array of values which must map to the given enum.
@@ -44,11 +41,17 @@ export class SBRadioButtonComponent {
   @Input()
   public model: any;
 
+  // The analytics event to send when clicked
   @Input()
   public analyticsEvent: string;
 
+  // The analytics category to use
   @Input()
   public analyticsCategory: string;
+
+  // The label for this button list
+  @Input()
+  public label: string;
 
   private _name: string;
 
