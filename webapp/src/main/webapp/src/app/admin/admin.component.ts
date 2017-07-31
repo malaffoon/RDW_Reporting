b17245f7defaa0ecec5ca130658c003bc603d445
Chris Dietz <cdietz@fairwaytech.com>
2017-07-31 08:42:11 -0700

    wire up upload component, archive service

--- webapp/src/main/webapp/src/app/admin/admin.component.ts
+++ webapp/src/main/webapp/src/app/admin/admin.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-import { AdminService } from "./admin.service";
-import { WarehouseImport } from "./warehouse-import.model";
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from './admin.service';
+import { WarehouseImport } from './warehouse-import.model';
+import { FileUploader } from 'ng2-file-upload';
+
+const URL = '/api/studentGroups/';
 
 @Component({
   selector: 'admin',
@@ -9,6 +12,7 @@ import { WarehouseImport } from "./warehouse-import.model";
 export class AdminComponent implements OnInit {
 
   warehouseImports: WarehouseImport[] = [];
+  public uploader: FileUploader = new FileUploader({ url: URL });
 
   constructor(private adminService: AdminService) {
 
--- webapp/src/main/webapp/src/app/admin/admin.module.ts
+++ webapp/src/main/webapp/src/app/admin/admin.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "../shared/common.module";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FormsModule } from "@angular/forms";
-import { DataTableModule, SharedModule } from "primeng/primeng";
-import { PopoverModule } from "ngx-bootstrap/popover";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '../shared/common.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { DataTableModule, SharedModule } from 'primeng/primeng';
+import { PopoverModule } from 'ngx-bootstrap/popover';
 import { Angulartics2Module } from 'angulartics2';
-import { AdminComponent } from "./admin.component";
-import { AdminService } from "./admin.service";
+import { AdminComponent } from './admin.component';
+import { AdminService } from './admin.service';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { AdminService } from "./admin.service";
     DataTableModule,
     SharedModule,
     PopoverModule,
+    FileUploadModule,
     Angulartics2Module.forChild()
   ],
   exports: [ AdminComponent ],
