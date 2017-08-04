7757ab1a424db576a1e4b61163e08e148d8f06f1
Will <wtritch@users.noreply.github.com>
2017-08-04 09:04:27 -0700

    Feature/spring batch report generation (#356)
    * DWR-652 Initial Spring batch report generation framework

--- common/src/test/resources/common-integration-test-data.sql
+++ common/src/test/resources/common-integration-test-data.sql
@@ -12,7 +12,8 @@ insert into gender (id, code) values
   (-1, 'g1');
 
 insert into student (id, ssid, last_or_surname, first_name, gender_id, gender_code, birthday, import_id, updated, migrate_id) values
-  (-1, 'student1_ssid', 'student1_lastName', 'student1_firstName', -1, 'g1', '2017-01-01 00:00:00.000000', -1, '2017-07-18 20:14:34.000000', -1);
+  (-1, 'student1_ssid', 'student1_lastName', 'student1_firstName', -1, 'g1', '2017-01-01 00:00:00.000000', -1, '2017-07-18 20:14:34.000000', -1),
+  (-2, 'student2_ssid', 'student2_lastName', 'student2_firstName', -1, 'g1', '2017-01-01 00:00:00.000000', -1, '2017-07-18 20:14:34.000000', -1);
 
 insert into asmt (id, type_id, natural_id, grade_id, grade_code, subject_id, school_year, name, label, version,
   claim1_score_code, claim2_score_code, claim3_score_code, claim4_score_code,
@@ -20,7 +21,10 @@ insert into asmt (id, type_id, natural_id, grade_id, grade_code, subject_id, sch
   (-1, 1, 'ica1', -1, 'g1', 1, 2017, 'ica1', 'ica1', 'v1', 'ica_claim1', 'ica_claim2', 'ica_claim3', 'ica_claim4', 100, 200, 300, 400, 500, -1, '2017-07-18 20:14:34.000000', -1),
   (-2, 2, 'iab1', -2, 'g2', 1, 2017, 'iab1', 'iab1', 'v1', null, null, null, null, 1, null, 2, null, 3, -1, '2017-07-18 20:14:34.000000', -1),
   (-3, 2, 'iab2', -1, 'g1', 1, 2016, 'iab2', 'iab2', 'v1', null, null, null, null, 1, null, 2, null, 3, -1, '2017-07-18 20:14:34.000000', -1),
-  (-4, 2, 'iab3', -1, 'g1', 1, 2015, 'iab3', 'iab3', 'v1', null, null, null, null, 1, null, 2, null, 3, -1, '2017-07-18 20:14:34.000000', -1);
+  (-4, 2, 'iab3', -1, 'g1', 1, 2015, 'iab3', 'iab3', 'v1', null, null, null, null, 1, null, 2, null, 3, -1, '2017-07-18 20:14:34.000000', -1),
+  (-5, 1, 'ica2', -1, 'g1', 2, 2017, 'ica2', 'ica2', 'v1', 'ica_claim1', 'ica_claim2', 'ica_claim3', 'ica_claim4', 100, 200, 300, 400, 500, -1, '2017-07-18 20:14:34.000000', -1),
+  (-6, 2, 'iab4', -1, 'g1', 2, 2017, 'iab2', 'iab4', 'v1', null, null, null, null, 1, null, 2, null, 3, -1, '2017-07-18 20:14:34.000000', -1),
+  (-7, 2, 'iab5', -1, 'g1', 2, 2017, 'iab3', 'iab5', 'v1', null, null, null, null, 1, null, 2, null, 3, -1, '2017-07-18 20:14:34.000000', -1);
 
 insert into exam (id, type_id, grade_id, grade_code, student_id, school_id, opportunity, iep, lep, section504, economic_disadvantage,
   school_year, asmt_id, asmt_version, completeness_id, completeness_code, administration_condition_id, administration_condition_code, session_id,
@@ -37,4 +41,10 @@ insert into exam (id, type_id, grade_id, grade_code, student_id, school_id, oppo
   (-5, 2, -2, 'g2', -1, -10, 1, 0, 0, 0, 0, 2017, -3, 'v1', 2, 'Complete', 1, 'Valid', 'session5', 2100, 21, 2, '2017-01-03 00:00:00.000000', null, null, null, null, null, null, null, null, null, null, null, null, -1, '2017-07-18 20:14:34.000000', -1),
   (-6, 2, -2, 'g2', -1, -10, 1, 0, 0, 0, 0, 2017, -3, 'v1', 2, 'Complete', 1, 'Valid', 'session6', 2100, 21, 2, '2017-01-04 00:00:00.000000', null, null, null, null, null, null, null, null, null, null, null, null, -1, '2017-07-18 20:14:34.000000', -1),
   (-7, 2, -1, 'g1', -1, -10, 1, 0, 0, 0, 0, 2016, -4, 'v1', 2, 'Complete', 1, 'Valid', 'session7', 2100, 21, 2, '2017-01-05 00:00:00.000000', null, null, null, null, null, null, null, null, null, null, null, null, -1, '2017-07-18 20:14:34.000000', -1),
-  (-8, 2, -1, 'g1', -1, -10, 1, 0, 0, 0, 0, 2016, -4, 'v1', 2, 'Complete', 1, 'Valid', 'session8', 2100, 21, 2, '2017-01-06 00:00:00.000000', null, null, null, null, null, null, null, null, null, null, null, null, -1, '2017-07-18 20:14:34.000000', -1);
+  (-8, 2, -1, 'g1', -1, -10, 1, 0, 0, 0, 0, 2016, -4, 'v1', 2, 'Complete', 1, 'Valid', 'session8', 2100, 21, 2, '2017-01-06 00:00:00.000000', null, null, null, null, null, null, null, null, null, null, null, null, -1, '2017-07-18 20:14:34.000000', -1),
+  (-9, 1, -2, 'g2', -1, -10, 0, 0, 0, 0, 0, 2017, -5, 'v1', 2, 'Complete', 1, 'Valid', 'session9', 2000, 20, 1, '2017-01-01 00:00:00.000000', 1, 100, 10, 2, 200, 20, 3, 300, 30, 4, 400, 40, -1, '2017-07-18 20:14:34.000000', -1),
+  (-10, 2, -1, 'g1', -1, -10, 1, 0, 0, 0, 0, 2017, -6, 'v1', 2, 'Complete', 1, 'Valid', 'session10', 2100, 21, 2, '2017-01-05 00:00:00.000000', null, null, null, null, null, null, null, null, null, null, null, null, -1, '2017-07-18 20:14:34.000000', -1),
+  (-11, 2, -1, 'g1', -1, -10, 1, 0, 0, 0, 0, 2017, -7, 'v1', 2, 'Complete', 1, 'Valid', 'session11', 2100, 21, 2, '2017-01-06 00:00:00.000000', null, null, null, null, null, null, null, null, null, null, null, null, -1, '2017-07-18 20:14:34.000000', -1),
+  (-12, 1, -2, 'g2', -2, -10, 0, 0, 0, 0, 0, 2017, -5, 'v1', 2, 'Complete', 1, 'Valid', 'session9', 2000, 20, 1, '2017-01-01 00:00:00.000000', 1, 100, 10, 2, 200, 20, 3, 300, 30, 4, 400, 40, -1, '2017-07-18 20:14:34.000000', -1),
+  (-13, 2, -1, 'g1', -2, -10, 1, 0, 0, 0, 0, 2017, -6, 'v1', 2, 'Complete', 1, 'Valid', 'session10', 2100, 21, 2, '2017-01-05 00:00:00.000000', null, null, null, null, null, null, null, null, null, null, null, null, -1, '2017-07-18 20:14:34.000000', -1),
+  (-14, 2, -1, 'g1', -2, -10, 1, 0, 0, 0, 0, 2017, -7, 'v1', 2, 'Complete', 1, 'Valid', 'session11', 2100, 21, 2, '2017-01-06 00:00:00.000000', null, null, null, null, null, null, null, null, null, null, null, null, -1, '2017-07-18 20:14:34.000000', -1);
