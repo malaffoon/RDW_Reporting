46f667424af1b68fd6803493233ecc4020e10aa8
Alla Gorina <agorina@fairwaytech.com>
2017-08-04 11:43:24 -0700

    DWR-545 RDW_Admin API Validate

--- webapp/src/main/webapp/src/app/admin/admin.module.ts
+++ webapp/src/main/webapp/src/app/admin/admin.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '../shared/common.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { DataTableModule, SharedModule } from 'primeng/primeng';
-import { PopoverModule } from 'ngx-bootstrap/popover';
-import { Angulartics2Module } from 'angulartics2';
-import { AdminComponent } from './admin.component';
-import { AdminService } from './admin.service';
-import { FileUploadModule } from 'ng2-file-upload';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "../shared/common.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import { DataTableModule, SharedModule } from "primeng/primeng";
+import { PopoverModule } from "ngx-bootstrap/popover";
+import { Angulartics2Module } from "angulartics2";
+import { AdminComponent } from "./admin.component";
+import { StudentGroupService } from "./student-group.service";
+import { FileUploadModule } from "ng2-file-upload";
 
 @NgModule({
   declarations: [
@@ -26,7 +26,8 @@ import { FileUploadModule } from 'ng2-file-upload';
     Angulartics2Module.forChild()
   ],
   exports: [ AdminComponent ],
-  providers: [ AdminService ]
+  providers: [ StudentGroupService ]
 })
+
 export class AdminModule {
 }
