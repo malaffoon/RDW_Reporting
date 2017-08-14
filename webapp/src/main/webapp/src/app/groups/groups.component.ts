f50659b9f60b284f29ee571b89ed08b717cd4419
Brandon Savage <bsavage@fairwaytech.com>
2017-08-14 10:36:14 -0700

    Now authorizing the route and menu option against "GROUP_READ". Now subscribing to route param changes so that the selections / data can be updated automagically as the params change (i.e. back / forward buttons). Now handling the error case in which a user has GROUP_READ but doesn't have access to any schools.

--- webapp/src/main/webapp/src/app/groups/groups.component.ts
+++ webapp/src/main/webapp/src/app/groups/groups.component.ts
@@ -23,18 +23,21 @@ export class GroupsComponent implements OnInit {
 
   ngOnInit() {
     this.filterOptions = this.route.snapshot.data[ "filterOptions" ];
-
     this.query = new GroupQuery(this.filterOptions.subjects);
-    let params: any = this.route.snapshot.params;
 
-    if(this.filterOptions.schools.length > 0) {
+    if(this.filterOptions.schools.length == 0)
+      return;
+
+    this.route.params.subscribe(p => {
+      let params:any = p;
+
       this.query.school = this.filterOptions.schools.find(school => school.id == params.schoolId) || this.filterOptions.schools[ 0 ];
-    }
 
-    this.query.schoolYear = +params.schoolYear || this.filterOptions.schoolYears[ 0 ];
-    this.query.subject =  params.subject || this.filterOptions.subjects[ 0 ];
+      this.query.schoolYear = +params.schoolYear || this.filterOptions.schoolYears[ 0 ];
+      this.query.subject =  params.subject || this.filterOptions.subjects[ 0 ];
 
-    this.updateResults();
+      this.updateResults();
+    });
   }
 
   updateRoute() {
@@ -44,9 +47,7 @@ export class GroupsComponent implements OnInit {
       subject: this.query.subject
     };
 
-    this.router.navigate([ params ], { relativeTo: this.route }).then(() => {
-      this.updateResults();
-    });
+    this.router.navigate([ params ], { relativeTo: this.route });
   }
 
   updateResults() {
