057cbe0da504a4fed12779cff208fd1790b93374
Jeff Treuting <jtreuting@fairwaytech.com>
2017-08-28 15:26:38 -0700

    Replace existing message in a way that allows the browser to remove it and show a visual reference to the suer that it was replaced by their action

--- webapp/src/main/webapp/src/app/shared/notification/notification.component.ts
+++ webapp/src/main/webapp/src/app/shared/notification/notification.component.ts
@@ -36,9 +36,11 @@ export class NotificationComponent {
    * @param notification A notification
    */
   private onNotification(notification) {
-    if (!this.notifications.some(x => _.isEqual(x, notification))) {
-      this.notifications.push(notification);
-    }
+    // if the same message is being generated again then remove it so a new one can be added
+    _.remove(this.notifications, x => _.isEqual(x, notification));
+
+    // add a brief pause to this so that Angular can remove it from display and a visual reference is made so the user knows the existing was replaced
+    setTimeout(() => this.notifications.push(notification), 50);
   }
 
 }
