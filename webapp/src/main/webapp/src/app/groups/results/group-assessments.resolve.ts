e4c1f78e44c89331265a4ce51d9e15613f2619f7
esotrope <llunsford@fairwaytech.com>
2017-09-01 16:32:58 -0700

    removes delay put in for testing

--- webapp/src/main/webapp/src/app/groups/results/group-assessments.resolve.ts
+++ webapp/src/main/webapp/src/app/groups/results/group-assessments.resolve.ts
@@ -6,8 +6,7 @@ import { GroupAssessmentService } from "./group-assessment.service";
 @Injectable()
 export class GroupAssessmentsResolve implements Resolve<any> {
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any>|Promise<any>|any {
-    return this.service.getMostRecentAssessment(route.params[ "groupId" ], route.params[ "schoolYear" ])
-      .delay(new Date(Date.now() + 4000));
+    return this.service.getMostRecentAssessment(route.params[ "groupId" ], route.params[ "schoolYear" ]);
   }
 
   constructor(private service: GroupAssessmentService) {
