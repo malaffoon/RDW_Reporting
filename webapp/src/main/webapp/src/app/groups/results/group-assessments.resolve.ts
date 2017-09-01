c3c4b36e732d2054c69a1b490a04036d19e79ed5
esotrope <llunsford@fairwaytech.com>
2017-09-01 16:31:39 -0700

    adds user guide link url

--- webapp/src/main/webapp/src/app/groups/results/group-assessments.resolve.ts
+++ webapp/src/main/webapp/src/app/groups/results/group-assessments.resolve.ts
@@ -6,7 +6,8 @@ import { GroupAssessmentService } from "./group-assessment.service";
 @Injectable()
 export class GroupAssessmentsResolve implements Resolve<any> {
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any>|Promise<any>|any {
-    return this.service.getMostRecentAssessment(route.params[ "groupId" ], route.params[ "schoolYear" ]);
+    return this.service.getMostRecentAssessment(route.params[ "groupId" ], route.params[ "schoolYear" ])
+      .delay(new Date(Date.now() + 4000));
   }
 
   constructor(private service: GroupAssessmentService) {
