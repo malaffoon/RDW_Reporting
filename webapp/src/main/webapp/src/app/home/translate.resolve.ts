d0998d912c1571fdfe36edfe04f916f3282d7c7e
Brandon Savage <bsavage@fairwaytech.com>
2017-09-19 15:40:27 -0700

    Now returning a translateService.get as a resolve instead of the RdwTranslateLoader observable in order to check that the translate service is done loading prior to rendering content.

--- webapp/src/main/webapp/src/app/home/translate.resolve.ts
+++ webapp/src/main/webapp/src/app/home/translate.resolve.ts
@@ -2,16 +2,17 @@ import { Injectable } from "@angular/core";
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
 import { RdwTranslateLoader } from "../shared/rdw-translate-loader";
+import { TranslateService } from "@ngx-translate/core";
 
 /**
  * Resolver that only resolves once the translate getTranslation is complete.
  */
 @Injectable()
 export class TranslateResolve implements Resolve<string> {
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any>|Promise<any>|any {
-    return this.service.observable;
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<string>|Promise<string>|string {
+    return this.service.get("welcome");
   }
 
-  constructor(private service: RdwTranslateLoader) {
+  constructor(private service: TranslateService) {
   }
 }
