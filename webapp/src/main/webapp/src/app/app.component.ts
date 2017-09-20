d455d29b1e38b587e785432d2bad80ee96b25802
Brandon Savage <bsavage@fairwaytech.com>
2017-09-20 11:50:10 -0700

    Updated SamlUserDetailsService to no longer throw an exception when no permissions are found, and instead to return a User with no permissions.  This allows the app to continue to load the user into the SecurityContextHolder so that logout can be made possible through the angular app.
    Updated access-denied to page to current implementation in Reporting.

--- webapp/src/main/webapp/src/app/app.component.ts
+++ webapp/src/main/webapp/src/app/app.component.ts
@@ -3,15 +3,16 @@ import { Component } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
 import { UserService } from "./user/user.service";
 import { User } from "./user/model/user.model";
+import { Router } from "@angular/router";
+
+const RequiredPermissions = [ "GROUP_WRITE" ];
+const SupportedLanguages = [ 'en', 'ja' ];
 
 @Component({
   selector: 'app-component',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-
-  public userHasAccess:boolean = undefined;
-
   private _user: User;
 
   get user(): User {
@@ -21,14 +22,15 @@ export class AppComponent {
   /*
   Even though the angulartics2GoogleAnalytics variable is not explicitly used, without it analytics data is not sent to the service
    */
-  constructor(public translate: TranslateService, private _userService: UserService,
+  constructor(public translate: TranslateService,
+              private _userService: UserService,
+              private router: Router,
               private angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) {
 
-    let languages = [ 'en', 'ja' ];
-    let defaultLanguage = languages[ 0 ];
-    translate.addLangs(languages);
+    let defaultLanguage = SupportedLanguages[ 0 ];
+    translate.addLangs(SupportedLanguages);
     translate.setDefaultLang(defaultLanguage);
-    translate.use(languages.indexOf(translate.getBrowserLang()) != -1 ? translate.getBrowserLang() : defaultLanguage);
+    translate.use(SupportedLanguages.indexOf(translate.getBrowserLang()) != -1 ? translate.getBrowserLang() : defaultLanguage);
 
   }
 
@@ -36,11 +38,14 @@ export class AppComponent {
     this._userService.getCurrentUser().subscribe(user => {
 
       this._user = user;
+      let userHasAccess = user.permissions.some(permission => RequiredPermissions.indexOf(permission) !== -1);
 
-      this.userHasAccess = user.permissions.some(permission => permission == "GROUP_WRITE");
+      if (!userHasAccess) {
+        this.router.navigate([ 'access-denied' ]);
+      }
 
-      if (window['ga'] && user.configuration && user.configuration.analyticsTrackingId) {
-        window['ga']('create', user.configuration.analyticsTrackingId, 'auto');
+      if (window[ 'ga' ] && user.configuration && user.configuration.analyticsTrackingId) {
+        window[ 'ga' ]('create', user.configuration.analyticsTrackingId, 'auto');
       }
     });
   }
