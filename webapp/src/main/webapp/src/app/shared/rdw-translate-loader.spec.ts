b49cf131bf273b730f6ca401fc1217a92afa0dfc
Will <wtritch@users.noreply.github.com>
2017-11-06 16:46:17 -0800

    Support configurable languages for the reporting webapp UI (#570)
    * Support configurable languages for the reporting webapp UI

--- webapp/src/main/webapp/src/app/shared/rdw-translate-loader.spec.ts
+++ webapp/src/main/webapp/src/app/shared/rdw-translate-loader.spec.ts
@@ -1,43 +1,63 @@
 import { RdwTranslateLoader } from "./rdw-translate-loader";
-import { Http, RequestOptionsArgs, RequestOptions, Response, ResponseOptions } from "@angular/http";
+import { Http, RequestOptions, RequestOptionsArgs, Response, ResponseOptions } from "@angular/http";
 import { Observable } from "rxjs";
-
-let apiObserver;
-let uiObserver;
-
-let uiResponse;
-let apiResponse;
-
-beforeEach(() =>{
-  let apiMockData = { 'welcome.title': 'Hello from api', 'welcome.message': 'The api welcomes you!' };
-  let uiMockData = {
-    'welcome': {
-      'title': 'Hello from UI',
-      'message': 'The UI welcomes you!',
-      'prompt': 'You have been prompted from the UI!'
-    },
-    'labels': {
-      'combo-box': 'Select'
-    }
-  };
-
-  apiResponse = new Response(new ResponseOptions({
-    body: JSON.stringify(apiMockData)
-  }));
-
-  uiResponse = new Response(new ResponseOptions({
-    body: JSON.stringify(uiMockData)
-  }));
-
-});
+import Spy = jasmine.Spy;
 
 describe('RdwTranslateLoader', () => {
+
+  let apiObserver;
+  let uiObserver;
+
+  let uiResponse;
+  let apiResponse;
+
+  let mockHttp: Http;
+
+  beforeEach(() =>{
+    let apiMockData = {
+      'welcome': {
+        'title': 'Hello from API',
+        'message': 'The API welcomes you!'
+      }
+    };
+    let uiMockData = {
+      'welcome': {
+        'title': 'Hello from UI',
+        'message': 'The UI welcomes you!',
+        'prompt': 'You have been prompted from the UI!'
+      },
+      'labels': {
+        'combo-box': 'Select'
+      }
+    };
+
+    apiResponse = new Response(new ResponseOptions({
+      body: JSON.stringify(apiMockData)
+    }));
+
+    uiResponse = new Response(new ResponseOptions({
+      body: JSON.stringify(uiMockData)
+    }));
+
+    mockHttp = new Http(null, new RequestOptions());
+    let getMethod: Spy = spyOn(mockHttp, "get");
+    getMethod.and.callFake((url: string, options?: RequestOptionsArgs): Observable<any> => {
+      let observable;
+
+      observable = url.indexOf('.json') === -1
+        ? new Observable(observer => apiObserver = observer)
+        : new Observable(observer => uiObserver = observer);
+
+      return observable;
+    });
+  });
+
   it('should merge api and ui translations', () => {
-    let fixture = new RdwTranslateLoader(new MockHttp());
+    let fixture = new RdwTranslateLoader(mockHttp);
     fixture.getTranslation('en').subscribe(actual => {
 
-      expect(actual['welcome']['title']).toBe('Hello from api');
-      expect(actual['welcome']['message']).toBe('The api welcomes you!');
+      expect(actual['welcome']['title']).toBe('Hello from API');
+      expect(actual['welcome']['message']).toBe('The API welcomes you!');
       expect(actual['welcome']['prompt']).toBe('You have been prompted from the UI!');
       expect(actual['labels']['combo-box']).toBe('Select');
     });
@@ -47,12 +67,10 @@ describe('RdwTranslateLoader', () => {
 
     apiObserver.next(apiResponse);
     apiObserver.complete();
-  })
-});
+  });
 
-describe('RdwTranslateLoader', () => {
   it('should fall back to ui translations when no api translations are available', () => {
-    let fixture = new RdwTranslateLoader(new MockHttp());
+    let fixture = new RdwTranslateLoader(mockHttp);
     let response = new Response(new ResponseOptions({
       body: JSON.stringify({})
     }));
@@ -70,21 +88,7 @@ describe('RdwTranslateLoader', () => {
 
     uiObserver.next(uiResponse);
     uiObserver.complete();
-  })
+  });
+
 });
 
-class MockHttp extends Http {
-  constructor() {
-    super(null, new RequestOptions());
-  }
-
-  get(url: string, options?: RequestOptionsArgs): Observable<any> {
-    let observable;
-
-    observable = url.indexOf('.json') === -1
-      ? new Observable(observer => apiObserver = observer)
-      : new Observable(observer => uiObserver = observer);
-
-    return observable;
-  }
-}
