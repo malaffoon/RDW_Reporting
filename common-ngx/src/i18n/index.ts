266dea7f9ed9d2094ef6528f28fc9a50143d56e8
wtritch <wtritchler@fairwaytech.com>
2017-11-08 13:19:27 -0800

    Revert "Support multiple languages in the admin webapp"
    This reverts commit e68af11

--- common-ngx/src/i18n/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './rdw-i18n.module';
-export * from './rdw-translate-loader';
--- webapp/src/main/webapp/src/app/shared/rdw-translate-loader.ts
+++ webapp/src/main/webapp/src/app/shared/rdw-translate-loader.ts
@@ -1,14 +1,12 @@
+import { Observable, Observer } from "rxjs";
 import { JsonUnFlat } from "json-unflat";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { Http } from "@angular/http";
 import * as _ from "lodash";
 import { Injectable } from "@angular/core";
-import { TranslateLoader } from "@ngx-translate/core";
-import { Observable } from "rxjs/Observable";
-import { Observer } from "rxjs/Observer";
 
 @Injectable()
-export class RdwTranslateLoader implements TranslateLoader {
+export class RdwTranslateLoader {
 
   private embeddedLanguages: string[] = ["en"];
 
@@ -18,11 +16,11 @@ export class RdwTranslateLoader implements TranslateLoader {
   private apiLoader = new TranslateHttpLoader(this.http, '/api/translations/', '');
   private uiLoader = new TranslateHttpLoader(this.http, '/assets/i18n/', '.json');
 
-  getTranslation(lang: string): Observable<any> {
+  getTranslation(lang: string) {
     let uiObservable = this.embeddedLanguages.indexOf(lang) >= 0 ?
         this.uiLoader.getTranslation(lang) : Observable.of({});
 
-    let apiObservable = this.apiLoader.getTranslation(lang).catch(() => Observable.of({}));
+    let apiObservable = this.apiLoader.getTranslation(lang).catch(res => Observable.of({}));
 
     let translateObserver: Observer<any>;
     let observable = new Observable<any>(observer => translateObserver = observer);
