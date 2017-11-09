65d97fdec52bff0a538378a4c83f2d1070d5a990
esotrope <llunsford@fairwaytech.com>
2017-11-09 12:54:11 -0800

    fixes test issues

--- common-ngx/demo/esm/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: `<sb-typeahead></sb-typeahead>`
-})
-export class AppComponent {
-  public header: string = 'UMD Demo';
-}
