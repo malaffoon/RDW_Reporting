07ec1bde9dfe564c42c35fc1f76654dd0699182e
Alla Gorina <agorina@fairwaytech.com>
2017-12-13 12:03:49 -0800

    DWR-1205 More tests and bug fixes.

--- olap/src/test/resources/OlapCodesSetup.sql
+++ olap/src/test/resources/OlapCodesSetup.sql
@@ -1,5 +1,5 @@
-INSERT INTO grade (id, code, name) VALUES (-99,'99','99-grade'),(-98 ,'98','98-grade');
-INSERT INTO completeness (id, code) VALUES (-99, 'c_code-99'), (-98, 'c_code-98');
-INSERT INTO administration_condition VALUES (-99,'ac_code-99'),(-98,'cc_code-98'),(-97,'cc_code-97'),(-96,'cc_code-96t');
-INSERT INTO ethnicity VALUES (-99,'ethnicity-99'),(-98,'ethnicity-98');
-INSERT INTO gender VALUES (-99,'gender-99'),(-98,'gender-98');
+INSERT INTO grade (id, code, name) VALUES (-9,'-9','grade-9'),(-8 ,'-8','grade-8');
+INSERT INTO completeness (id, code) VALUES (-9, 'c_code-9'), (-8, 'c_code-8');
+INSERT INTO administration_condition VALUES (-9,'ac_code-9'),(-8,'cc_code-8'),(-97,'cc_code-97'),(-96,'cc_code-96t');
+INSERT INTO ethnicity VALUES (-9,'ethnicity-9'),(-8,'ethnicity-8');
+INSERT INTO gender VALUES (-9,'gender-9'),(-8,'gender-8');
