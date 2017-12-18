e4a4fa0875d305a8d5584778d9b3bd110031e000
Devin Snyder <33040425+dsnyder-fairway@users.noreply.github.com>
2017-12-18 09:06:21 -0800

    redirect to access-denied when users do not have permissions (#672)

--- common-ngx/src/security/authorization.can-activate.ts
+++ common-ngx/src/security/authorization.can-activate.ts
@@ -1,7 +1,8 @@
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from "@angular/router";
-import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+import { Inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 import { AuthorizationService } from "./authorization.service";
+import { AccessDeniedRoute } from "./routing-authorization.can-activate";
 
 /**
  * Allows access to a route if a user has at least one of the permissions in ActivatedRouteSnapshot.data.permissions.
@@ -9,11 +10,18 @@ import { AuthorizationService } from "./authorization.service";
 @Injectable()
 export class AuthorizationCanActivate implements CanActivate {
 
-  constructor(private authorizationService: AuthorizationService) {
+  constructor(@Inject(AccessDeniedRoute) private accessDeniedRoute: string,
+              private authorizationService: AuthorizationService,
+              private router: Router) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.authorizationService.hasAnyPermission(route.data[ 'permissions' ]);
+    return this.authorizationService.hasAnyPermission(route.data[ 'permissions' ]).map((permission) => {
+        if (!permission) {
+          this.router.navigate([ this.accessDeniedRoute ]);
+        }
+        return permission;
+      }
+    );
   }
-
 }
