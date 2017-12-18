819f8c042d10b7d7e0e85013a83065c618bde32b
Alla Gorina <agorina@fairwaytech.com>
2017-12-18 11:00:48 -0800

    DWR-1205 Remove unintended changes

--- webapp/src/main/webapp/src/app/aggregate-report/aggregate-reports.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'aggregate-reports',
-  templateUrl: './aggregate-reports.component.html',
-})
-export class AggregateReportsComponent {
-
-}
--- webapp/src/main/webapp/src/app/aggregate-report/results/aggregate-reports-results.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'aggregate-reports-results',
-  templateUrl: './aggregate-reports-results.component.html',
-})
-export class AggregateReportsResultsComponent {
-
-}
