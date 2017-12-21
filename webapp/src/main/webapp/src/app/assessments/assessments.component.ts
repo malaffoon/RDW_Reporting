bbcc825ad0cd51f321518434fcacf3fc5cd02a8b
Devin Snyder <33040425+dsnyder-fairway@users.noreply.github.com>
2017-12-21 11:30:12 -0800

    handle schools which are not properly configured or do not contain as… (#687)

--- webapp/src/main/webapp/src/app/assessments/assessments.component.ts
+++ webapp/src/main/webapp/src/app/assessments/assessments.component.ts
@@ -30,7 +30,7 @@ export class AssessmentsComponent implements OnInit {
   @Input()
   set assessmentExams(value: AssessmentExam[]) {
     this._assessmentExams = value;
-    this.showOnlyMostRecent = true;
+    this.showOnlyMostRecent = value.length > 0;
     this._hasInitialAssessment = (value != null && value.length != 0);
   }
 
