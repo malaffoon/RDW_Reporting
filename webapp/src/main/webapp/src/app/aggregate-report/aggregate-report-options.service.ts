c96552510c572a404d944c83ada3a4ba735d1f9f
esotrope <llunsford@fairwaytech.com>
2018-01-31 14:11:50 -0800

    fixes references to option fields in component template

--- webapp/src/main/webapp/src/app/aggregate-report/aggregate-report-options.service.ts
+++ webapp/src/main/webapp/src/app/aggregate-report/aggregate-report-options.service.ts
@@ -15,41 +15,7 @@ export class AggregateReportOptionsService {
   }
 
   getReportOptions(): Observable<AggregateReportOptions> {
-    return this.dataService.get(`${ServiceRoute}/reportOptions`)
-      .map(options => <AggregateReportOptions>{
-        assessmentGrades: options.assessmentGrades,
-        assessmentTypes: options.assessmentTypes,
-        ethnicities: options.ethnicities,
-        genders: options.genders,
-        schoolYears: options.schoolYears,
-        subjects: options.subjects,
-
-        // The goal is to make these data driven and to include policy information in the admin condition
-        // (e.g. appliesToAssessmentType: ['iab', 'ica'] or appliesToInterim: true)
-        interimAdministrationConditions: [
-          {id: 2, code: 'SD'},
-          {id: 3, code: 'NS'}
-        ],
-        summativeAdministrationConditions: [
-          {id: 1, code: 'Valid'},
-          {id: 4, code: 'IN'}
-        ],
-        completenesses: [
-          {id: 2, code: 'Complete'},
-          {id: 1, code: 'Partial'}
-        ],
-
-        // These may be data-driven later and contain undefined (not-specified) as a value
-        migrantStatuses: [true, false],
-        ieps: [true, false],
-        plan504s: [true, false],
-        limitedEnglishProficiencies: [true, false],
-        economicDisadvantages: [true, false],
-
-        dimensionTypes: options.dimensionTypes,
-
-        statewideReporter: options.statewideReporter
-      });
+    return this.dataService.get(`${ServiceRoute}/reportOptions`);
   }
 
 }
