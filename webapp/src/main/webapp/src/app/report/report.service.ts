131b5bb7e878732626134c3ceed014a40ce081ca
esotrope <llunsford@fairwaytech.com>
2018-02-05 18:08:29 -0800

    fixes request signatures to backend report processor

--- webapp/src/main/webapp/src/app/report/report.service.ts
+++ webapp/src/main/webapp/src/app/report/report.service.ts
@@ -129,8 +129,7 @@ export class ReportService {
     return this.dataService.get(`${ServiceRoute}/reports/${reportId}`, {
       headers: new Headers({
         'Accept': 'application/json',
-      }),
-      responseType: ResponseContentType.Json
+      })
     }).catch(ResponseUtils.throwError);
   }
 
@@ -142,9 +141,6 @@ export class ReportService {
    */
   public getAggregateReportAsSpreadsheet(reportId: number): Observable<Download> {
     return this.dataService.get(`${ServiceRoute}/reports/${reportId}`, {
-      headers: new Headers({
-        'Accept': 'text/csv',
-      }),
       responseType: ResponseContentType.Blob
     }).catch(ResponseUtils.throwError);
   }
