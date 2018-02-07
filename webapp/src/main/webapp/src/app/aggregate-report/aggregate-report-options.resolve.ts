a325f1b6cefe42c0f7445ad5d198ac5b30986862
esotrope <llunsford@fairwaytech.com>
2018-02-06 17:29:35 -0800

    fixes where option sorting goes

--- webapp/src/main/webapp/src/app/aggregate-report/aggregate-report-options.resolve.ts
+++ webapp/src/main/webapp/src/app/aggregate-report/aggregate-report-options.resolve.ts
@@ -5,9 +5,6 @@ import { AggregateReportOptionsService } from "./aggregate-report-options.servic
 import { AggregateReportOptions } from "./aggregate-report-options";
 
 
-// Used to hotfix natural order of completeness and strict booleans not being in "affirmative-first" order
-const IdDescending = (a, b) => b.id - a.id;
-
 /**
  * This resolver is responsible for fetching assessment items for a student and exam.
  */
@@ -18,24 +15,7 @@ export class AggregateReportOptionsResolve implements Resolve<AggregateReportOpt
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<AggregateReportOptions> {
-    return this.service.getReportOptions().map(options => <AggregateReportOptions>{
-      assessmentGrades: options.assessmentGrades,
-      assessmentTypes: options.assessmentTypes,
-      completenesses: options.completenesses.sort(IdDescending),
-      dimensionTypes: options.dimensionTypes,
-      economicDisadvantages: options.economicDisadvantages.sort(IdDescending),
-      ethnicities: options.ethnicities,
-      genders: options.genders,
-      individualEducationPlans: options.individualEducationPlans.sort(IdDescending),
-      interimAdministrationConditions: options.interimAdministrationConditions,
-      limitedEnglishProficiencies: options.limitedEnglishProficiencies.sort(IdDescending),
-      migrantStatuses: options.migrantStatuses.sort(IdDescending),
-      section504s: options.section504s.sort(IdDescending),
-      schoolYears: options.schoolYears,
-      statewideReporter: options.statewideReporter,
-      subjects: options.subjects,
-      summativeAdministrationConditions: options.summativeAdministrationConditions
-    });
+    return this.service.getReportOptions();
   }
 
 }
