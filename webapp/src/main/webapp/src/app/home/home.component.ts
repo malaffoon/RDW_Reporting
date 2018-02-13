11cb9f58debc4263e0962fc43e50ef1c429cc560
esotrope <llunsford@fairwaytech.com>
2018-02-13 10:19:26 -0800

    cleans up system news and removes default

--- webapp/src/main/webapp/src/app/home/home.component.ts
+++ webapp/src/main/webapp/src/app/home/home.component.ts
@@ -1,25 +1,17 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { User } from "../user/model/user.model";
-import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'home',
-  templateUrl: './home.component.html'
+  templateUrl: 'home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   user: User;
-  systemNewsHtml: string;
 
-  constructor(private route: ActivatedRoute,
-              private translate: TranslateService) {
+  constructor(route: ActivatedRoute) {
+    this.user = route.snapshot.data[ 'user' ];
   }
 
-  ngOnInit() {
-    this.user = this.route.snapshot.data[ 'user' ];
-    this.translate.get('html.system-news').subscribe(result => {
-      this.systemNewsHtml = result;
-    });
-  }
 }
