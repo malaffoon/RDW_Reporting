0e612e2eb7c10b7be559740ec7eccb39b5fca9ca
esotrope <llunsford@fairwaytech.com>
2018-02-21 11:39:25 -0800

    changes homepage header over to page-heading component

---
+++ webapp/src/main/webapp/src/app/shared/layout/page-heading.component.ts
@@ -0,0 +1,25 @@
+import { Component, ContentChild, ElementRef, Input, TemplateRef } from "@angular/core";
+import { Utils } from "../support/support";
+
+@Component({
+  selector: 'page-heading',
+  templateUrl: 'page-heading.component.html',
+  host: {
+    'class': 'page-heading'
+  }
+})
+export class PageHeading {
+
+  @ContentChild('heading')
+  heading: TemplateRef<ElementRef>;
+
+  @ContentChild('content')
+  content: TemplateRef<ElementRef>;
+
+  ngOnInit(): void {
+    if (Utils.isNullOrUndefined(this.heading)) {
+      throw new Error('heading template must not be null or undefined');
+    }
+  }
+
+}
