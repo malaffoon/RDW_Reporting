a3de82864fb7155e3317e60368c9bb2bff340073
esotrope <llunsford@fairwaytech.com>
2018-02-22 21:04:33 -0800

    fixes duplicate calls to districts

--- webapp/src/main/webapp/src/app/school-grade/organization.service.ts
+++ webapp/src/main/webapp/src/app/school-grade/organization.service.ts
@@ -2,15 +2,14 @@ import { Injectable } from "@angular/core";
 import { UserService } from "../user/user.service";
 import { School } from "../user/model/school.model";
 import { Observable } from "rxjs/Observable";
-import "rxjs/add/observable/forkJoin";
 import { CachingDataService } from "../shared/data/caching-data.service";
+import { ReportingServiceRoute } from "../shared/service-route";
+import { forkJoin } from "rxjs/observable/forkJoin";
+import { map, publishReplay, refCount } from "rxjs/operators";
 
-const ServiceRoute = '/reporting-service';
 
 @Injectable()
 export class OrganizationService {
-
-  private schoolsWithDistricts: Observable<School[]>;
 
   constructor(private userService: UserService,
               private dataService: CachingDataService) {
@@ -23,26 +22,43 @@ export class OrganizationService {
    * @returns {Observable<School[]>}
    */
   getSchoolsWithDistricts(): Observable<School[]> {
-    if (this.schoolsWithDistricts) {
-      return this.schoolsWithDistricts;
-    }
-    return this.schoolsWithDistricts = Observable
-      .forkJoin(
-        this.userService.getCurrentUser(),
-        this.getDistrictNamesById()
-      )
-      .map(response => {
-        let [ user, districts ] = response;
-        return user.schools.map(school => {
-          school.districtName = districts.get(school.districtId);
-          return school;
+    return forkJoin(
+      this.getSchools(),
+      this.getDistrictNamesById()
+    ).pipe(
+      map(([ schools, districts ]) => {
+        return schools.map(school => {
+          const result = new School();
+          result.id = school.id;
+          result.name = school.name;
+          result.schoolGroupId = school.schoolGroupId;
+          result.districtId = school.districtId;
+          result.districtName = districts.get(school.districtId);
+          result.districtGroupId = school.districtGroupId;
+          return result;
         });
-      });
+      }),
+      // when combined these operators act as a cache of the first valid result of the observable
+      publishReplay(1),
+      refCount()
+    );
   }
 
-  private getDistrictNamesById() {
-    return this.dataService
-      .get(`${ServiceRoute}/organizations/districts`)
-      .map(districts => new Map<number, string>(districts.map(x => [ x.id, x.name ])));
+  private getSchools(): Observable<School[]> {
+    // TODO use /reporting-service/organizations/schools instead of user payload to untie user from reporting-service
+    return this.userService.getCurrentUser().pipe(
+      map(user => user.schools)
+    );
   }
+
+  private getDistricts(): Observable<any[]> {
+    return this.dataService.get(`${ReportingServiceRoute}/organizations/districts`);
+  }
+
+  private getDistrictNamesById(): Observable<Map<number, string>> {
+    return this.getDistricts().pipe(
+      map(districts => new Map<number, string>(districts.map(district => <any>[ district.id, district.name ])))
+    );
+  }
+
 }
