ed1b2ef8efe85ad7437875e26149193ee7c2bfbc
esotrope <llunsford@fairwaytech.com>
2018-02-27 12:51:52 -0800

    simplifies interface for alerts

--- webapp/src/main/webapp/src/app/shared/embargo/embargo-alert.component.ts
+++ webapp/src/main/webapp/src/app/shared/embargo/embargo-alert.component.ts
@@ -1,44 +1,26 @@
-import { Component, Input } from "@angular/core";
+import { Component } from "@angular/core";
 import { Utils } from "../support/support";
 import { ReportingEmbargoService } from "./reporting-embargo.service";
 import { AggregateEmbargoService } from "./aggregate-embargo.service";
-
-
-export const ReportingEmbargoAlertSource = 'reporting';
-export const AggregateEmbargoAlertSource = 'aggregate';
 
 @Component({
   selector: 'embargo-alert',
   templateUrl: 'embargo-alert.component.html'
 })
-export class EmbargoAlertComponent {
+export class EmbargoAlert {
 
-  private _showAlert: boolean = undefined;
-  private _source: string = ReportingEmbargoAlertSource;
+  private _showAlert: boolean;
 
-  constructor(private reportingEmbargoService: ReportingEmbargoService,
-              private aggregateEmbargoService: AggregateEmbargoService){
+  constructor(private service: ReportingEmbargoService | AggregateEmbargoService) {
   }
 
   get showAlert(): boolean {
     return this._showAlert;
   }
 
-  @Input()
-  set source(value: string) {
-    this._source = value;
-  }
-
   ngOnInit(): void {
     if (Utils.isNullOrUndefined(this._showAlert)) {
-      switch (this._source) {
-        case ReportingEmbargoAlertSource:
-          this.reportingEmbargoService.isEmbargoed().subscribe(embargoed => this._showAlert = embargoed);
-          break;
-        case AggregateEmbargoAlertSource:
-          this.aggregateEmbargoService.isEmbargoed().subscribe(embargoed => this._showAlert = embargoed);
-          break;
-      }
+      this.service.isEmbargoed().subscribe(embargoed => this._showAlert = embargoed);
     }
   }
 
--- webapp/src/main/webapp/src/app/shared/embargo/embargo.module.ts
+++ webapp/src/main/webapp/src/app/shared/embargo/embargo.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { TranslateModule } from "@ngx-translate/core";
-import { EmbargoAlertComponent } from "./embargo-alert.component";
 import { AggregateEmbargoService } from "./aggregate-embargo.service";
 import { ReportingEmbargoService } from "./reporting-embargo.service";
+import { AggregateEmbargoAlert } from "./aggregate-embargo-alert.component";
+import { ReportingEmbargoAlert } from "./reporting-embargo-alert.component";
 
 @NgModule({
   declarations: [
-    EmbargoAlertComponent
+    AggregateEmbargoAlert,
+    ReportingEmbargoAlert
   ],
   imports: [
     BrowserModule,
@@ -18,7 +20,8 @@ import { ReportingEmbargoService } from "./reporting-embargo.service";
     ReportingEmbargoService
   ],
   exports: [
-    EmbargoAlertComponent
+    AggregateEmbargoAlert,
+    ReportingEmbargoAlert
   ]
 })
 export class CommonEmbargoModule {
