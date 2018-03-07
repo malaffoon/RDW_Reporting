f5cf99cfa795807b8603e58d8764a3393db85b57
esotrope <llunsford@fairwaytech.com>
2018-03-06 17:05:00 -0800

    code duplicative but it fixes the display issue

--- webapp/src/main/webapp/src/app/aggregate-report/results/aggregate-report-table.component.ts
+++ webapp/src/main/webapp/src/app/aggregate-report/results/aggregate-report-table.component.ts
@@ -114,11 +114,13 @@ export class AggregateReportTableComponent implements OnInit {
   }
 
   get performanceLevelDisplayType(): string {
+    console.log('get', this._performanceLevelDisplayType)
     return this._performanceLevelDisplayType;
   }
 
   @Input()
   set performanceLevelDisplayType(value: string) {
+    console.log('set', value);
     this._performanceLevelDisplayType = PerformanceLevelDisplayTypes.valueOf(value);
   }
 
