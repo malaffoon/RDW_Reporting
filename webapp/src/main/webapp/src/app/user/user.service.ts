11a6fb429ba1f70e3d96977f897d895834124d10
esotrope <llunsford@fairwaytech.com>
2018-03-08 17:30:42 -0800

    simplify services

--- webapp/src/main/webapp/src/app/user/user.service.ts
+++ webapp/src/main/webapp/src/app/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 import { User } from "./user";
 import { CachingDataService } from "../shared/data/caching-data.service";
-import { map, publishReplay, refCount } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { ReportingServiceRoute } from '../shared/service-route';
 
 @Injectable()
@@ -17,9 +17,7 @@ export class UserService {
         firstName: serverUser.firstName,
         lastName: serverUser.lastName,
         permissions: serverUser.permissions
-      }),
-      publishReplay(1),
-      refCount()
+      })
     )
   }
 
