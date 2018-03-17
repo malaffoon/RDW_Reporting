33d098e080ae586b3ed85faa5449a2e3149f8fa1
esotrope <llunsford@fairwaytech.com>
2018-03-16 17:17:42 -0700

    removes optional status of assessmentLabel field on report row

--- webapp/src/main/webapp/src/app/aggregate-report/results/aggregate-report-item.ts
+++ webapp/src/main/webapp/src/app/aggregate-report/results/aggregate-report-item.ts
@@ -6,7 +6,7 @@ import { Organization } from "../../shared/organization/organization";
 export class AggregateReportItem {
   itemId: number;
   assessmentId: number;
-  assessmentLabel?: string;
+  assessmentLabel: string;
   assessmentGradeCode: string;
   subjectCode: string;
   schoolYear: number;
