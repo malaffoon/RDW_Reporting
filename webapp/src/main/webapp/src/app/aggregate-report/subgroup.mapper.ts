ef94c1d908ba8a04bf8761950390c9ff75468067
Devin Snyder <33040425+dsnyder-fairway@users.noreply.github.com>
2018-04-05 11:49:33 -0700

    created dimension-prefix for subgroup rows. Expanded ELAS -> English … (#1181)

--- webapp/src/main/webapp/src/app/aggregate-report/subgroup.mapper.ts
+++ webapp/src/main/webapp/src/app/aggregate-report/subgroup.mapper.ts
@@ -13,7 +13,7 @@ export class SubgroupMapper {
   createOverallSubgroupFiltersListItem(): SubgroupFiltersListItem {
     return {
       id: '',
-      name: this.translate.instant('common.dimension.Overall'),
+      name: this.translate.instant('common.dimension-short-name.Overall'),
       value: undefined
     };
   }
@@ -63,7 +63,7 @@ export class SubgroupMapper {
     const suffix = value && dimension ? `: ${translate(dimension.getTranslationCode(value))}` : '';
     return {
       id: `${type}:${value}`,
-      name: `${translate(`common.dimension.${type}`)}${suffix}`,
+      name: `${translate(`common.dimension-short-name.${type}`)}${suffix}`,
       type: type,
       code: value
     };
