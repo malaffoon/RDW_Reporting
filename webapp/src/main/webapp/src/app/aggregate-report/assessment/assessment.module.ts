c32dca7a0ea33eaad372f7b1b7ae55c855155f39
esotrope <llunsford@fairwaytech.com>
2018-04-27 17:11:46 -0700

    adds webapp api

--- webapp/src/main/webapp/src/app/aggregate-report/assessment/assessment.module.ts
+++ webapp/src/main/webapp/src/app/aggregate-report/assessment/assessment.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { AssessmentDefinitionService } from "./assessment-definition.service";
 import { AssessmentDefinitionResolve } from "./assessment-definition.resolve";
+import { AssessmentService } from './assessment.service';
 
 @NgModule({
   providers: [
     AssessmentDefinitionService,
-    AssessmentDefinitionResolve
+    AssessmentDefinitionResolve,
+    AssessmentService
   ]
 })
 export class AssessmentModule {
