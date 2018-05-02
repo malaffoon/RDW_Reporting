71f91d64e5fa6384adc1eed9b85c6e593c0cb9cf
Will <wtritch@users.noreply.github.com>
2018-05-02 15:22:44 -0700

    Bump schema version and account for translation schema changes. (#1259)

--- common/src/test/resources/translations.sql
+++ common/src/test/resources/translations.sql
@@ -1,6 +1,6 @@
-insert into translation (namespace, language_code, label_code, label) values
-  ('backend', 'eng', 'msg1', 'English Message 1'),
-  ('backend', 'eng', 'msg2', 'English Message 2'),
-  ('backend', 'jpn', 'msg1', 'Japanese Message 1'),
-  ('backend', 'jpn', 'msg2', 'Japanese Message 2'),
-  ('frontend', 'eng', 'msg3', 'English Message 3');
+insert into accommodation_translation (language_code, label_code, label) values
+  ('eng', 'msg1', 'English Message 1'),
+  ('eng', 'msg2', 'English Message 2'),
+  ('jpn', 'msg1', 'Japanese Message 1'),
+  ('jpn', 'msg2', 'Japanese Message 2'),
+  ('eng', 'msg3', 'English Message 3');
