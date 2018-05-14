d6413858fcf7a1bea0fa79808a4849e9b89d6b97
Jeff Treuting <jtreuting@fairwaytech.com>
2018-05-14 12:28:26 -0700

    single View IAB Dashboard on the landing page defaulting to first group sort groups by name at service for consistency on landing page and group dashboard

--- webapp/src/main/webapp/src/app/groups/group.service.ts
+++ webapp/src/main/webapp/src/app/groups/group.service.ts
@@ -4,6 +4,8 @@ import { ReportingServiceRoute } from '../shared/service-route';
 import { map } from 'rxjs/operators';
 import { Group } from '../groups/group';
 import { Observable } from 'rxjs/Observable';
+import { byString } from '@kourge/ordering/comparator';
+import { ordering } from '@kourge/ordering';
 
 @Injectable()
 export class GroupService {
@@ -13,14 +15,16 @@ export class GroupService {
 
   getGroups(): Observable<Group[]> {
     return this.dataService.get(`${ReportingServiceRoute}/groups`).pipe(
-      map(serverGroups => serverGroups.map(serverGroup => {
-        return <Group>{
-          id: serverGroup.id,
-          name: serverGroup.name,
-          schoolName: serverGroup.schoolName,
-          schoolId: serverGroup.schoolId,
-          subjectCode: serverGroup.subjectCode || 'ALL'
-        };
+      map(serverGroups => serverGroups
+        .sort(ordering(byString).on<any>(x => x.name).compare)
+        .map(serverGroup => {
+          return <Group>{
+            id: serverGroup.id,
+            name: serverGroup.name,
+            schoolName: serverGroup.schoolName,
+            schoolId: serverGroup.schoolId,
+            subjectCode: serverGroup.subjectCode || 'ALL'
+          };
       }))
     );
   }
