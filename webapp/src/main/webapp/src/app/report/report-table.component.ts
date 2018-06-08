687de109f9a049f5ebce19b13f33301b9029ca6a
esotrope <llunsford@fairwaytech.com>
2018-06-07 17:29:27 -0700

    fixes sort order of report table columns

--- webapp/src/main/webapp/src/app/report/report-table.component.ts
+++ webapp/src/main/webapp/src/app/report/report-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Report } from './report.model';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'report-table',
@@ -8,17 +9,48 @@ import { Report } from './report.model';
 export class ReportTableComponent {
 
   @Input()
-  reports: Report[];
+  subjects: string[];
+
+  private _reportViews: ReportView[];
 
   columns: Column[] = [
-    new Column({id: 'label-header', field: 'label'}),
-    new Column({id: 'report-type-header', field: 'reportType'}),
-    new Column({id: 'assessment-type-header', field: 'assessmentType'}),
-    new Column({id: 'subject-header', field: 'subjectCodes'}),
-    new Column({id: 'school-year-header', field: 'schoolYears'}),
-    new Column({id: 'status-header', field: 'status'}),
-    new Column({id: 'created-header', field: 'created'})
+    new Column({ id: 'label-header', field: 'report.label' }),
+    new Column({ id: 'report-type-header', field: 'report.reportType', sortField: 'translatedReportType' }),
+    new Column({
+      id: 'assessment-type-header',
+      field: 'report.assessmentTypeCode',
+      sortField: 'translatedAssessmentType'
+    }),
+    new Column({ id: 'subject-header', field: 'report.subjectCodes', sortField: 'translatedSubjectCodes' }),
+    new Column({ id: 'school-year-header', field: 'report.schoolYears' }),
+    new Column({ id: 'status-header', field: 'report.status' }),
+    new Column({ id: 'created-header', field: 'report.created' })
   ];
+
+  constructor(private translateService: TranslateService) {
+  }
+
+  get reportViews(): ReportView[] {
+    return this._reportViews;
+  }
+
+  @Input()
+  set reports(reports: Report[]) {
+    const translate = code => this.translateService.instant(code);
+    this._reportViews = (reports || []).map(report => <ReportView>{
+      report: report,
+      translatedAssessmentType: translate(report.assessmentTypeCode
+        ? `common.assessment-type.${report.assessmentTypeCode}.short-name`
+        : 'reports.all'
+      ),
+      // Not ideal, we should not have empty/null subject codes for reports,
+      // it should be the subjects the report was created for at the time of creation
+      translatedSubjectCodes: report.subjectCodes.length
+        ? report.subjectCodes.map(code => translate(`common.subject.${code}.name`)).join(', ')
+        : translate('common.subject.ALL.name'),
+      translatedReportType: translate(`reports.report-type.${report.reportType}`)
+    });
+  }
 
 }
 
@@ -26,9 +58,18 @@ export class ReportTableComponent {
 class Column {
   id: string;
   field: string;
+  sortField: string;
 
-  constructor({ id, field }) {
+  constructor({ id, field, sortField = '' }) {
     this.id = id;
     this.field = field;
+    this.sortField = sortField ? sortField : field;
   }
+}
+
+interface ReportView {
+  report: Report;
+  translatedAssessmentType: string;
+  translatedSubjectCodes: string;
+  translatedReportType: string;
 }
