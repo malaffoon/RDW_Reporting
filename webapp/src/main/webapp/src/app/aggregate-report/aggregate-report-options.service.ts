6cb9874d9a7e8c15c63914b965379e3955da9075
Jeff Treuting <jtreuting@fairwaytech.com>
2018-06-22 13:16:04 -0700

    dont' include longitudinal and target report types if summative is not an assessment type

--- webapp/src/main/webapp/src/app/aggregate-report/aggregate-report-options.service.ts
+++ webapp/src/main/webapp/src/app/aggregate-report/aggregate-report-options.service.ts
@@ -36,7 +36,9 @@ export class AggregateReportOptionsService {
         dimensionTypes: serverOptions.dimensionTypes.concat(),
         interimAdministrationConditions: serverOptions.interimAdministrationConditions.concat(),
         queryTypes: [ 'Basic', 'FilteredSubgroup' ],
-        reportTypes: [ AggregateReportType.GeneralPopulation, AggregateReportType.LongitudinalCohort, AggregateReportType.Claim, AggregateReportType.Target ],
+        reportTypes: serverOptions.assessmentTypes.some(x => x.value == 'sum')
+          ? [ AggregateReportType.GeneralPopulation, AggregateReportType.LongitudinalCohort, AggregateReportType.Claim, AggregateReportType.Target ]
+          : [ AggregateReportType.GeneralPopulation, AggregateReportType.Claim ],
         schoolYears: serverOptions.schoolYears.concat(),
         statewideReporter: serverOptions.statewideReporter,
         subjects: serverOptions.subjects.concat(),
