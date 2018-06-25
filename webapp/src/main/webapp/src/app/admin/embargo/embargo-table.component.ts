4a49127dfd749134c30d4d9dc4b6714a150c1436
Jeff Treuting <jtreuting@fairwaytech.com>
2018-06-25 16:04:36 -0700

    config subjects for embargo (#1604)
    * config subjects for embargo
    
    * remove unused import
    
    * remove commented out code
    
    * minor format

--- webapp/src/main/webapp/src/app/admin/embargo/embargo-table.component.ts
+++ webapp/src/main/webapp/src/app/admin/embargo/embargo-table.component.ts
@@ -3,6 +3,7 @@ import { Embargo } from "./embargo";
 import { Toggle } from "./toggle.component";
 import { EmbargoToggleEvent } from "./embargo-toggle-event";
 import { EmbargoScope } from "./embargo-scope.enum";
+import { SubjectService } from '../../subject/subject.service';
 
 @Component({
   selector: 'embargo-table',
@@ -22,8 +23,7 @@ export class EmbargoTable implements OnInit {
   @Output()
   toggle: EventEmitter<EmbargoToggleEvent> = new EventEmitter<EmbargoToggleEvent>();
 
-  // TODO:ConfigurableSubjects later this will be provided by a service and will be the actual subject codes: ELA, Math
-  subjectCodes: string[] = [ 'Math', 'ELA' ];
+  subjectCodes: string[] = [];
   columns: Column[];
 
   get hasMultipleEmbargoes(): boolean {
@@ -38,13 +38,20 @@ export class EmbargoTable implements OnInit {
     return this.overridingEmbargo && !this.overridingEmbargo.aggregateEnabled;
   }
 
+  constructor(private subjectService: SubjectService) {
+  }
+
   ngOnInit(): void {
-    this.columns = [
-      new Column({id: 'name'}),
-      ...this.getSubjectColumns(),
-      new Column({id: 'individualEnabled'}),
-      new Column({id: 'aggregateEnabled'})
-    ];
+    this.subjectService.getSubjectCodes().subscribe(subjectCodes => {
+      this.subjectCodes = subjectCodes;
+
+      this.columns = [
+        new Column({id: 'name'}),
+        ...this.getSubjectColumns(),
+        new Column({id: 'individualEnabled'}),
+        new Column({id: 'aggregateEnabled'})
+      ];
+    });
   }
 
   toggleIndividual(toggle: Toggle, embargo: Embargo): void {
@@ -55,6 +62,10 @@ export class EmbargoTable implements OnInit {
   toggleAggregate(toggle: Toggle, embargo: Embargo): void {
     this.toggleInternal(toggle, embargo, EmbargoScope.Aggregate, embargo.aggregateEnabled,
       this.overridingEmbargo ? this.overridingEmbargo.aggregateEnabled : undefined);
+  }
+
+  getExamCount(embargo: Embargo, subjectCode) {
+    return embargo.examCountsBySubject[subjectCode] || 0;
   }
 
   private getSubjectColumns(): Column[] {
@@ -102,9 +113,5 @@ class Column {
     if (code) {
       this.code = code;
     }
-  }
-
-  get serverSubjectCode() {
-    return this.code ? this.code.toUpperCase() : this.code;
   }
 }
