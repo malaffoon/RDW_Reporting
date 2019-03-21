d31e3e9c0c0bab30d6d3b9e5040a5bea091090fd
Mark Laffoon <laffoon@san.rr.com>
2019-03-21 06:47:46 -0700

    remove unused methods (#1796)

--- webapp/src/main/webapp/src/app/assessments/results/scale-score.service.spec.ts
+++ webapp/src/main/webapp/src/app/assessments/results/scale-score.service.spec.ts
@@ -1,108 +1,39 @@
-import { Assessment } from "../model/assessment.model";
-import { inject, TestBed } from "@angular/core/testing";
-import { ScaleScoreService } from "./scale-score.service";
-import { ExamStatisticsLevel } from "../model/exam-statistics.model";
+import { inject, TestBed } from '@angular/core/testing';
+import { ScaleScoreService } from './scale-score.service';
+import { ExamStatisticsLevel } from '../model/exam-statistics.model';
 
 describe('ScaleScoreService', () => {
-
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [
-        ScaleScoreService
-      ]
+      providers: [ScaleScoreService]
     });
   });
 
-  it('should return level 1 for score below minimum for ICA',
-    inject([ScaleScoreService], (service: ScaleScoreService) => {
+  it('should calculate proper distribution numbers when decimals below min', inject(
+    [ScaleScoreService],
+    (service: ScaleScoreService) => {
+      let level1 = new ExamStatisticsLevel();
+      level1.value = 68.75;
+      let level2 = new ExamStatisticsLevel();
+      level2.value = 14.5833;
+      let level3 = new ExamStatisticsLevel();
+      level3.value = 6.25;
+      let level4 = new ExamStatisticsLevel();
+      level4.value = 10.416;
 
-    let asmt = new Assessment();
-    asmt.type = 'ica';
-    asmt.cutPoints = [1000, 1100, 1200, 1300, 1400];
+      let original = [level1, level2, level3, level4];
 
-    let actual = service.calculateLevelNumber(asmt, 20, 1.0);
-    expect(actual).toBe(1);
-  }));
+      let actual = service.calculateDisplayScoreDistribution(original);
+      expect(actual[0]).toBe(66);
+      expect(actual[1]).toBe(14);
+      expect(actual[2]).toBe(6);
+      expect(actual[3]).toBe(10);
+    }
+  ));
 
-  it('should return level 4 for score above maximum for ICA',
-    inject([ScaleScoreService], (service: ScaleScoreService) => {
-
-    let asmt = new Assessment();
-    asmt.type = 'ica';
-    asmt.cutPoints = [1000, 1100, 1200, 1300, 1400];
-
-    let actual = service.calculateLevelNumber(asmt, 9000, 1.0);
-    expect(actual).toBe(4);
-  }));
-
-  it('should return level 2 for score in range for ICA',
-    inject([ScaleScoreService], (service: ScaleScoreService) => {
-
-    let asmt = new Assessment();
-    asmt.type = 'ica';
-    asmt.cutPoints = [1000, 1100, 1200, 1300, 1400];
-
-    let actual = service.calculateLevelNumber(asmt, 1150, 1.0);
-    expect(actual).toBe(2);
-  }));
-
-  it('should return level 1 for score far enough below cut point for IAB',
-    inject([ScaleScoreService], (service: ScaleScoreService) => {
-
-    let asmt = new Assessment();
-    asmt.type = 'iab';
-    asmt.cutPoints = [1000, 1100, 1200, 1300, 1400];
-
-    let actual = service.calculateLevelNumber(asmt, 1049, 100.0);
-    expect(actual).toBe(1);
-  }));
-
-  it('should return level 3 for score far enough above cut point for IAB',
-    inject([ScaleScoreService], (service: ScaleScoreService) => {
-
-    let asmt = new Assessment();
-    asmt.type = 'iab';
-    asmt.cutPoints = [1000, 1100, 1200, 1300, 1400];
-
-    let actual = service.calculateLevelNumber(asmt, 1351, 100);
-    expect(actual).toBe(3);
-  }));
-
-  it('should return level 2 for score in mid range for IAB',
-    inject([ScaleScoreService], (service: ScaleScoreService) => {
-
-    let asmt = new Assessment();
-    asmt.type = 'iab';
-    asmt.cutPoints = [1000, 1100, 1200, 1300, 1400];
-
-    let actual = service.calculateLevelNumber(asmt, 1150, 100.0);
-    expect(actual).toBe(2);
-  }));
-
-  it('should calculate proper distribution numbers when decimals below min',
-      inject([ScaleScoreService], (service: ScaleScoreService) => {
-
-        let level1 = new ExamStatisticsLevel();
-        level1.value = 68.75;
-        let level2 = new ExamStatisticsLevel();
-        level2.value = 14.5833;
-        let level3 = new ExamStatisticsLevel();
-        level3.value = 6.25;
-        let level4 = new ExamStatisticsLevel();
-        level4.value = 10.416;
-
-        let original = [level1, level2,  level3, level4];
-
-        let actual = service.calculateDisplayScoreDistribution(original);
-        expect(actual[0]).toBe(66);
-        expect(actual[1]).toBe(14);
-        expect(actual[2]).toBe(6);
-        expect(actual[3]).toBe(10);
-      }));
-
-  it('should calculate proper distribution numbers when 0, 1, 9',
-    inject([ScaleScoreService], (service: ScaleScoreService) => {
-
+  it('should calculate proper distribution numbers when 0, 1, 9', inject(
+    [ScaleScoreService],
+    (service: ScaleScoreService) => {
       let level1 = new ExamStatisticsLevel();
       level1.value = 0.0;
       let level2 = new ExamStatisticsLevel();
@@ -110,17 +41,18 @@ describe('ScaleScoreService', () => {
       let level3 = new ExamStatisticsLevel();
       level3.value = 90.0;
 
-      let original = [level1, level2,  level3];
+      let original = [level1, level2, level3];
 
       let actual = service.calculateDisplayScoreDistribution(original);
       expect(actual[0]).toBe(0);
       expect(actual[1]).toBe(10);
       expect(actual[2]).toBe(80);
-    }));
+    }
+  ));
 
-  it('should calculate proper distribution numbers all above min',
-    inject([ScaleScoreService], (service: ScaleScoreService) => {
-
+  it('should calculate proper distribution numbers all above min', inject(
+    [ScaleScoreService],
+    (service: ScaleScoreService) => {
       let level1 = new ExamStatisticsLevel();
       level1.value = 20;
       let level2 = new ExamStatisticsLevel();
@@ -130,18 +62,19 @@ describe('ScaleScoreService', () => {
       let level4 = new ExamStatisticsLevel();
       level4.value = 10;
 
-      let original = [level1, level2,  level3, level4];
+      let original = [level1, level2, level3, level4];
 
       let actual = service.calculateDisplayScoreDistribution(original);
       expect(actual[0]).toBe(20.0);
       expect(actual[1]).toBe(30.0);
       expect(actual[2]).toBe(40.0);
       expect(actual[3]).toBe(10.0);
-    }));
+    }
+  ));
 
-  it('should calculate proper distribution numbers with some below min',
-    inject([ScaleScoreService], (service: ScaleScoreService) => {
-
+  it('should calculate proper distribution numbers with some below min', inject(
+    [ScaleScoreService],
+    (service: ScaleScoreService) => {
       let level1 = new ExamStatisticsLevel();
       level1.value = 5;
       let level2 = new ExamStatisticsLevel();
@@ -151,7 +84,7 @@ describe('ScaleScoreService', () => {
       let level4 = new ExamStatisticsLevel();
       level4.value = 60;
 
-      let original = [level1, level2,  level3, level4];
+      let original = [level1, level2, level3, level4];
 
       let actual = service.calculateDisplayScoreDistribution(original);
 
@@ -159,11 +92,12 @@ describe('ScaleScoreService', () => {
       expect(actual[1]).toBe(28);
       expect(actual[2]).toBe(3);
       expect(actual[3]).toBe(52);
-    }));
+    }
+  ));
 
-  it('should calculate proper distribution numbers when the originals sum to more than 100',
-    inject([ScaleScoreService], (service: ScaleScoreService) => {
-
+  it('should calculate proper distribution numbers when the originals sum to more than 100', inject(
+    [ScaleScoreService],
+    (service: ScaleScoreService) => {
       let level1 = new ExamStatisticsLevel();
       level1.value = 20;
       let level2 = new ExamStatisticsLevel();
@@ -173,7 +107,7 @@ describe('ScaleScoreService', () => {
       let level4 = new ExamStatisticsLevel();
       level4.value = 12;
 
-      let original = [level1, level2,  level3, level4];
+      let original = [level1, level2, level3, level4];
 
       let actual = service.calculateDisplayScoreDistribution(original);
 
@@ -181,11 +115,12 @@ describe('ScaleScoreService', () => {
       expect(actual[1]).toBe(30);
       expect(actual[2]).toBe(39);
       expect(actual[3]).toBe(11);
-    }));
+    }
+  ));
 
-  it('should calculate proper distribution numbers when the originals sum to zero',
-    inject([ScaleScoreService], (service: ScaleScoreService) => {
-
+  it('should calculate proper distribution numbers when the originals sum to zero', inject(
+    [ScaleScoreService],
+    (service: ScaleScoreService) => {
       let level1 = new ExamStatisticsLevel();
       level1.value = 0;
       let level2 = new ExamStatisticsLevel();
@@ -195,7 +130,7 @@ describe('ScaleScoreService', () => {
       let level4 = new ExamStatisticsLevel();
       level4.value = 0;
 
-      let original = [level1, level2,  level3, level4];
+      let original = [level1, level2, level3, level4];
 
       let actual = service.calculateDisplayScoreDistribution(original);
 
@@ -203,5 +138,6 @@ describe('ScaleScoreService', () => {
       expect(actual[1]).toBe(0);
       expect(actual[2]).toBe(0);
       expect(actual[3]).toBe(0);
-    }));
+    }
+  ));
 });
