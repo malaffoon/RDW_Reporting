c9c5b01eb349cf432faf15b2a3fd8a4d1bd7785d
esotrope <llunsford@fairwaytech.com>
2019-03-21 17:28:01 -0700

    adds "effective report languages" with english default

--- webapp/src/main/webapp/src/app/app-settings.ts
+++ webapp/src/main/webapp/src/app/app-settings.ts
@@ -1,5 +1,7 @@
+/**
+ * Represents all of the reporting system settings
+ */
 export interface ApplicationSettings {
-
   readonly irisVendorId: string;
   readonly analyticsTrackingId: string;
   readonly interpretiveGuideUrl: string;
@@ -10,7 +12,9 @@ export interface ApplicationSettings {
   readonly uiLanguages: string[];
   readonly transferAccess: boolean;
   readonly percentileDisplayEnabled: boolean;
+  /** @deprecated use {@link ExamSearchFilterService} */
   readonly lepEnabled: boolean;
+  /** @deprecated use {@link ExamSearchFilterService} */
   readonly elasEnabled: boolean;
   readonly targetReport: TargetReportSettings;
 }
@@ -18,4 +22,36 @@ export interface ApplicationSettings {
 export interface TargetReportSettings {
   readonly insufficientDataCutoff: number;
   readonly minimumStudentCount: number;
+}
+
+export function toApplicationSettings(
+  serverSettings: any
+): ApplicationSettings {
+  const accessDeniedUrl = serverSettings.accessDeniedUrl.startsWith('redirect:')
+    ? serverSettings.accessDeniedUrl.substring(9)
+    : undefined;
+
+  return {
+    accessDeniedUrl,
+    analyticsTrackingId: serverSettings.analyticsTrackingId,
+    elasEnabled: serverSettings.englishLanguageAcquisitionStatusEnabled,
+    interpretiveGuideUrl: serverSettings.interpretiveGuideUrl,
+    irisVendorId: serverSettings.irisVendorId,
+    lepEnabled: serverSettings.limitedEnglishProficienciesEnabled,
+    minItemDataYear: serverSettings.minItemDataYear,
+    percentileDisplayEnabled: serverSettings.percentileDisplayEnabled,
+    reportLanguages: serverSettings.effectiveReportLanguages,
+    state: {
+      code: serverSettings.state.code,
+      name: serverSettings.state.name
+    },
+    targetReport: {
+      insufficientDataCutoff:
+        serverSettings.targetReport.insufficientDataCutoff,
+      minimumStudentCount: serverSettings.targetReport.minNumberOfStudents
+    },
+    transferAccess: serverSettings.transferAccessEnabled,
+    uiLanguages: serverSettings.uiLanguages,
+    userGuideUrl: serverSettings.userGuideUrl
+  };
 }
