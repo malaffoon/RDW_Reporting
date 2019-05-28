a562e6cd5bb2620998471e11e331dd6f23cc2c88
esotrope <llunsford@fairwaytech.com>
2019-05-28 15:37:15 -0700

    makes date comparison null-safe

--- webapp/src/main/webapp/src/app/shared/support/support.spec.ts
+++ webapp/src/main/webapp/src/app/shared/support/support.spec.ts
@@ -1,5 +1,6 @@
 import {
   deepEqualsIgnoringNullAndFalse,
+  equalDate,
   flattenJsonObject,
   Utils
 } from './support';
@@ -71,5 +72,37 @@ describe('deepEqualsIgnoringNullAndFalse', () => {
         }
       )
     ).toBe(true);
+  });
+});
+
+describe('equalDate', () => {
+  it('should be true when instances identical', () => {
+    const date = new Date();
+    expect(equalDate(date, date)).toBe(true);
+  });
+
+  it('should be true when the same date', () => {
+    const a = new Date(1, 2, 3);
+    const b = new Date(1, 2, 3);
+    expect(equalDate(a, b)).toBe(true);
+  });
+
+  it('should be false when one is null', () => {
+    const date = new Date();
+    expect(equalDate(date, null)).toBe(false);
+    expect(equalDate(null, date)).toBe(false);
+  });
+
+  it('should be false when has a different value for any field', () => {
+    const date = new Date(1, 2, 3);
+    expect(equalDate(date, new Date(1, 2, 100))).toBe(false);
+    expect(equalDate(date, new Date(1, 100, 3))).toBe(false);
+    expect(equalDate(date, new Date(100, 2, 3))).toBe(false);
+  });
+
+  it('should ignore time', () => {
+    const a = new Date(1, 2, 3, 4, 5, 6);
+    const b = new Date(1, 2, 3, 5, 6, 7);
+    expect(equalDate(a, b)).toBe(true);
   });
 });
