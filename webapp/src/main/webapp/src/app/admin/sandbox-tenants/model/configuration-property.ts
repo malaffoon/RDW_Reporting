dd18ad4b3ec62257dde57269c0c1944a2a9b3e1a
Ernie Munoz <emunoz@fairwaytech.com>
2019-05-29 11:36:44 -0700

    Moving formControlName computation to constructor

--- webapp/src/main/webapp/src/app/admin/sandbox-tenants/model/configuration-property.ts
+++ webapp/src/main/webapp/src/app/admin/sandbox-tenants/model/configuration-property.ts
@@ -3,18 +3,17 @@ export class ConfigurationProperty {
   originalValue: string;
   value: string;
   group?: string;
-
-  get formControlName(): string {
-    if (this.group) {
-      return `${this.group}.${this.key}`;
-    }
-    return this.key;
-  }
+  formControlName: string;
 
   constructor(key, value, group = null, originalVal = value) {
     this.key = key;
     this.value = value;
     this.originalValue = originalVal;
     this.group = group;
+
+    if (group) {
+      this.formControlName = `${group}.${key}`;
+    }
+    this.formControlName = key;
   }
 }
