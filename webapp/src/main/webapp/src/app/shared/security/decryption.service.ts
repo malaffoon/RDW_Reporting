e4c87c60e41774a33b1b5ff195b11810276056e1
Ernie Munoz <emunoz@fairwaytech.com>
2019-06-13 19:58:15 -0700

    Fixing decryption integration - adding support for text return type

--- webapp/src/main/webapp/src/app/shared/security/decryption.service.ts
+++ webapp/src/main/webapp/src/app/shared/security/decryption.service.ts
@@ -1,9 +1,11 @@
-import { Observable } from 'rxjs';
+import { Observable, pipe } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { AdminServiceRoute } from '../service-route';
 import { ResponseUtils } from '../response-utils';
 import { DataService } from '../data/data.service';
+import { HttpHeaders } from '@angular/common/http';
+import { ResponseContentType } from '@angular/http';
 
 const ResourceRoute = `${AdminServiceRoute}/config/decrypt`;
 
@@ -21,8 +23,13 @@ export class DecryptionService {
    * @param sandbox - The sandbox to create
    */
   decrypt(encryptedPassword: string): Observable<string> {
+    const httpOptions = {
+      responseType: 'text' as 'text'
+    };
     return this.dataService
-      .post(`${ResourceRoute}`, encryptedPassword)
+      .post(`${ResourceRoute}`, encryptedPassword, {
+        responseType: ResponseContentType.Text
+      })
       .pipe(catchError(ResponseUtils.throwError));
   }
 }
