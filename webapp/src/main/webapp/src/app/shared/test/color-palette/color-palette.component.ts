323166b0d5d662c5d276f73111c05ee0b6633cd8
Greg Charles <greg.charles@accenture.com>
2020-06-02 11:23:23 -0700

    RDW6 - req 1: import new UI Kit and palette page for testing

---
+++ webapp/src/main/webapp/src/app/shared/test/color-palette/color-palette.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color-palette',
+  templateUrl: './color-palette.component.html',
+  styleUrls: ['./color-palette.component.less']
+})
+export class ColorPaletteComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
